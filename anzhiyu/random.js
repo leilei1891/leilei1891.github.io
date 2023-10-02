87e18eee0792632d86ed8630e7315d0f8926c6ec
leilei1891 <leilei1891@outlook.com>
2023-10-02 13:37:18 +0800

    Site updated: 2023-10-02 13:37:16

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["p/2022/41149534/","p/2022/60258585/","p/2022/f607d2b5/","p/2022/250970e4/","p/2022/68adcea5/","p/2021/3f81df2e/","p/2021/ea8fe0c4/","p/2023/ea2479bd/","p/2023/7cb564cd/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["p/2022/41149534/","p/2022/60258585/","p/2022/f607d2b5/","p/2022/250970e4/","p/2022/68adcea5/","p/2021/3f81df2e/","p/2021/ea8fe0c4/","p/2023/7cb564cd/","p/2023/ea2479bd/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
